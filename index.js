e8db09b0479775d2f3205e6493b9300d6a5f25d5
hdaer <hardar333@hotmail.com>
2025-04-07 20:37:45 +0200

    uibuild

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const Person = require('./models/person')
+const cors = require('cors')
 
 const app = express()
 
@@ -10,6 +11,12 @@ morgan.token('body', (req) => JSON.stringify(req.body));
 app.use(express.json())
 app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+app.use(cors({
+    origin: 'http://localhost:3002', // Your frontend URL
+    methods: ['GET', 'POST', 'DELETE', 'PUT'],
+    allowedHeaders: ['Content-Type']
+}))
 
 //CREATE
 app.post('/api/persons', (request, response) => {
